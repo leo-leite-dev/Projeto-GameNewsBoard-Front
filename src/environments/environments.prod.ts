7e846c8a0dd5ca9b980254a988aa2a74da5c4252
Leonardo <leopassos.leite@gmail.com>
2025-07-18 20:35:54 -0300

    :fire: chore: remoção de headers ngrok e limpeza no proxy.prod.json

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://c399-2804-30c-1736-3200-f91c-d617-e383-9209.ngrok-free.app/api',
-  uploadsBaseUrl: 'https://c399-2804-30c-1736-3200-f91c-d617-e383-9209.ngrok-free.app/api',
+  apiBaseUrl: 'https://projeto-gamenewsboard-back.fly.dev/api',
+  uploadsBaseUrl: 'https://projeto-gamenewsboard-back.fly.dev/uploads'
 };
