9b72f8aa6369ca21dd84ae4e1911b3b2f456c889
Leonardo <leopassos.leite@gmail.com>
2025-08-03 05:55:33 -0300

    teste

--- src/app/shared/forms/input/input.component.ts
+++ src/app/shared/forms/input/input.component.ts
@@ -1,5 +1,16 @@
-import { Component, Input, Output, EventEmitter, forwardRef, OnInit, OnDestroy } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  forwardRef,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
+import {
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
 import { GenericModule } from '../../../../shareds/commons/GenericModule';
 import { ViewportService } from '../../services/commons/viewport.service';
 import { fromEvent, Subject } from 'rxjs';
@@ -23,11 +34,8 @@ export class InputComponent implements ControlValueAccessor, OnInit, OnDestroy {
   @Input() label: string = '';
   @Input() placeholder: string = '';
   @Input() type: string = 'text';
-  @Input() useMobileModal: boolean = true;
 
   @Output() valueChange = new EventEmitter<string>();
-  @Output() filterToggle = new EventEmitter<void>();
-  @Output() openMobileSearch = new EventEmitter<void>();
 
   value: string = '';
   showPassword = false;
@@ -81,13 +89,7 @@ export class InputComponent implements ControlValueAccessor, OnInit, OnDestroy {
     this.valueChange.emit(this.value);
   }
 
-  toggleFilter(): void {
-    this.filterToggle.emit();
-  }
-
-  openModal(): void {
-    if (this.isMobile && this.useMobileModal) {
-      this.openMobileSearch.emit();
-    }
+  onBlur(): void {
+    this.onTouchedFn();
   }
 }
